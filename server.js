e3e1f0dcc6ea9a976c062e4dc4f40a9e517bb51e
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-10-02 23:57:47 +0900

    rollDice追加

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const schema = buildSchema(`
         quoteOfTheDay: String
         random: Float!
         rollThreeDice: [Int]
+        rollDice(numDice: Int!, numSides: Int): [Int]
     }`
 )
 
@@ -22,6 +23,13 @@ const root = {
     rollThreeDice: () => {
         return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
     },
+    rollDice: ({numDice, numSides}) => {
+        let output = [];
+        for (let i = 0; i < numDice; i++) {
+          output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
+      }
 }
 
 // expressサーバ
